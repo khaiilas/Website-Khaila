d9d0828a94d8e64ab70e59e621dd4ff811c879ea
khaiilas <khaila.sukma53@gmail.com>
2024-11-25 20:10:04 +0700

    Add files via upload

---
+++ Web-Khaila/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar">
+            <a href="CV.php">CV</a>
+            <a href="Catalog.php">Catalog</a>
+            <a href="Chart.php">Chart</a>
+    </nav>
+    <section class="welcome">
+        <p>Selamat Datang!</p>
+    </section>
+</body>
+</html>
